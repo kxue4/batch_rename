5df1ed3cda67454054ad79ba10938c54e9b7f1b2
xuekaiwen <yixian.xue@gmail.com>
2025-06-04 14:14:35 +0800

    extracting variables out of the function

--- Kmoe下载批量改名.py
+++ Kmoe下载批量改名.py
@@ -1,41 +1,43 @@
 #!/usr/bin/env python
 # -*- coding: utf-8 -*-
 import os
-import re
 import shutil
 
+路径 = '/Users/*****/Downloads/test/'
+分割符 = ' '
 忽略文件列表 = '.DS_Store'
 序号类型列表 = ('話', '话', '卷')
 
 
-def 创建子文件夹(路径):
+def 创建子文件夹():
     for 文件 in os.listdir(路径):
-        if 文件 not in 忽略文件列表 and os.path.isfile(路径 + 文件): # 忽略系统文件，且忽略文件夹
-            分割符 = ' '
-            文件名 = '.'.join(文件.split('.')[0:-1])
-            漫画名称 = 文件名.split(分割符)[0]
-            os.makedirs(路径 + 漫画名称, exist_ok = True)
+        if 文件 not in 忽略文件列表 and os.path.isfile(路径 + 文件):  # 忽略系统文件，且忽略文件夹
+            try:
+                文件名 = '.'.join(文件.split('.')[0:-1])
+                漫画名称 = 文件名.split(分割符)[0]
+                os.makedirs(路径 + 漫画名称, exist_ok = True)
+            except:
+                print('【报错】', '【创建子文件夹】', 文件)
 
-def 移动文件(路径):
+def 移动文件():
     for 文件 in os.listdir(路径):
-        if 文件 not in 忽略文件列表 and os.path.isfile(路径 + 文件): # 忽略系统文件，且忽略文件夹
-            分割符 = ' '
-            文件名 = '.'.join(文件.split('.')[0:-1])
-            漫画名称 = 文件名.split(分割符)[0]
-            shutil.move(路径 + 文件, 路径 + 漫画名称 + '/')
+        if 文件 not in 忽略文件列表 and os.path.isfile(路径 + 文件):  # 忽略系统文件，且忽略文件夹
+            try:
+                文件名 = '.'.join(文件.split('.')[0:-1])
+                漫画名称 = 文件名.split(分割符)[0]
+                shutil.move(路径 + 文件, 路径 + 漫画名称 + '/')
+            except:
+                print('【报错】', '【移动文件】', 文件)
 
-def 重命名文件(路径):
-    for 漫画文件夹 in os.listdir(路径):
+def 重命名文件():
+    for 漫画文件夹 in os.listdir(路径):   # 忽略系统文件，且忽略文件夹
         if os.path.isdir(路径 + 漫画文件夹 + '/'):
             for 文件 in os.listdir(路径 + 漫画文件夹 + '/'):
                 if 文件 not in 忽略文件列表 and os.path.isfile(路径 + 漫画文件夹 + '/' + 文件):
-                    分割符 = ' '
                     格式 = 文件.split('.')[-1]
                     文件名 = '.'.join(文件.split('.')[0:-1])
-
                     try:
                         序号部分 = 文件名.split(分割符)[1]
-
                         for 序号类型 in 序号类型列表:
                             if 序号类型 in 序号部分:
                                 序号部分 = 序号部分.replace(序号类型,'')
@@ -47,12 +49,11 @@ def 重命名文件(路径):
 
 def main():
     print('---------------------------Running---------------------------')
-    路径 = '/Users/xuekaiwen/Downloads/test/'
-    创建子文件夹(路径)
+    创建子文件夹()
     print('子文件夹创建完毕!')
-    移动文件(路径)
+    移动文件()
     print('移动文件完毕!')
-    重命名文件(路径)
+    重命名文件()
     print('重命名文件完毕!')
     print('---------------------------Job Done--------------------------')
 
